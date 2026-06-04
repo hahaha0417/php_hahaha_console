ef5b40f5f2f5b1879f46ce952e8ebe970200d5ad
hahaha <hahaha0417@hotmail.com>
2026-06-04 22:44:19 +0800

    fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,5 +20,13 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         //
+		if ($this->app->runningInConsole()) {
+            $this->commands([
+                hahaha_cache_ai_context::class,
+                hahaha_cache_code_summary::class,
+                hahaha_cache_project_structure::class,
+                hahaha_command_db_table_enum_generate::class,
+            ]);
+        }
     }
 }
